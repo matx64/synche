699cf9700a07907b0fe75bbe27b3168ec5117f1c
Matheus Alves <mathalves@proton.me>
2025-04-26 13:37:28 -0300

    better error handling

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 use std::{net::Ipv4Addr, sync::Arc, time::Duration};
 use tokio::{net::UdpSocket, time::sleep};
 
-const MULTICAST_ADDR: &'static str = "239.0.0.1:9999";
+const MULTICAST_ADDR: &str = "239.0.0.1:9999";
 
 pub struct MultiCast {
     socket: Arc<UdpSocket>,
@@ -9,40 +9,50 @@ pub struct MultiCast {
 
 impl MultiCast {
     pub async fn new() -> Self {
-        let socket = Arc::new(UdpSocket::bind("0.0.0.0:9999").await.unwrap());
+        let socket = UdpSocket::bind("0.0.0.0:9999").await.unwrap();
         socket.set_multicast_loop_v4(false).unwrap();
         socket
-            .join_multicast_v4(Ipv4Addr::new(239, 0, 0, 1), Ipv4Addr::new(0, 0, 0, 0))
+            .join_multicast_v4(Ipv4Addr::new(239, 0, 0, 1), Ipv4Addr::UNSPECIFIED)
             .unwrap();
 
-        Self { socket }
+        Self {
+            socket: Arc::new(socket),
+        }
     }
 
     pub async fn listen(&self) {
         let socket = self.socket.clone();
         tokio::spawn(async move {
-            let mut buf = [0; 1024];
+            let mut buf = [0u8; 1024];
             loop {
                 match socket.recv_from(&mut buf).await {
                     Ok((len, src)) => {
                         let msg = String::from_utf8_lossy(&buf[..len]);
-                        println!("Multicast from {}: {}", src, msg);
-                        socket.send_to(b"pong", src).await.unwrap();
+                        println!("Received multicast from {}: {}", src, msg);
+
+                        if let Err(e) = socket.send_to(b"pong", src).await {
+                            eprintln!("Failed to send pong to {}: {}", src, e);
+                        }
                     }
-                    Err(e) => eprintln!("Receive error: {}", e),
+                    Err(e) => {
+                        eprintln!("Error receiving multicast: {}", e);
+                        break;
+                    }
                 }
             }
-        });
+        })
+        .await
+        .unwrap();
     }
 
     pub async fn presence(&self) {
         let socket = self.socket.clone();
         tokio::spawn(async move {
             loop {
-                if let Err(err) = socket.send_to(b"ping", MULTICAST_ADDR).await {
-                    eprintln!("Sending Presence error: {}", err);
+                match socket.send_to(b"ping", MULTICAST_ADDR).await {
+                    Ok(_) => println!("Presence ping sent."),
+                    Err(e) => eprintln!("Error sending presence: {}", e),
                 }
-                println!("Sent Presence");
                 sleep(Duration::from_secs(5)).await;
             }
         })
