42d6d057ceaac1771dad2264d037687b7613c235
Matheus Alves <mathalves@proton.me>
2025-06-04 14:07:38 -0300

    handle device disconnect

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 use local_ip_address::list_afinet_netifas;
 use std::{
-    collections::HashSet,
+    collections::HashMap,
     net::{IpAddr, SocketAddr},
     sync::{Arc, Mutex},
-    time::Duration,
+    time::{Duration, SystemTime},
 };
 use tokio::{io, net::UdpSocket};
 
 const BROADCAST_PORT: u16 = 8888;
 const BROADCAST_INTERVAL_SECS: u64 = 5;
+const DEVICE_TIMEOUT_SECS: u64 = 15;
+
+#[derive(Debug)]
+struct Device {
+    addr: SocketAddr,
+    last_seen: SystemTime,
+}
+
+impl Device {
+    pub fn new(addr: SocketAddr) -> Self {
+        Self {
+            addr,
+            last_seen: SystemTime::now(),
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -16,7 +32,7 @@ async fn main() -> io::Result<()> {
 
     let socket = Arc::new(UdpSocket::bind(&bind_addr).await?);
     socket.set_broadcast(true)?;
-    let devices = Arc::new(Mutex::new(HashSet::<SocketAddr>::new()));
+    let devices = Arc::new(Mutex::new(HashMap::<SocketAddr, Device>::new()));
 
     let send_task = tokio::spawn(send(socket.clone()));
     let recv_task = tokio::spawn(recv(socket, devices.clone()));
@@ -39,37 +55,49 @@ async fn send(socket: Arc<UdpSocket>) -> io::Result<()> {
     }
 }
 
-async fn recv(socket: Arc<UdpSocket>, devices: Arc<Mutex<HashSet<SocketAddr>>>) -> io::Result<()> {
+async fn recv(
+    socket: Arc<UdpSocket>,
+    devices: Arc<Mutex<HashMap<SocketAddr, Device>>>,
+) -> io::Result<()> {
     let ifas = list_afinet_netifas().unwrap();
     let mut buf = [0; 1024];
 
     loop {
         let (size, src_addr) = socket.recv_from(&mut buf).await?;
-        let msg = String::from_utf8_lossy(&buf[..size]);
 
+        let msg = String::from_utf8_lossy(&buf[..size]);
         if is_host(&ifas, src_addr.ip()) || msg != "ping" {
             continue;
         }
 
         {
             let mut devices = devices.lock().unwrap();
-            if !devices.contains(&src_addr) {
-                devices.insert(src_addr);
+            if devices.insert(src_addr, Device::new(src_addr)).is_none() {
                 println!("Device connected: {}", src_addr);
             }
         }
     }
 }
 
-async fn state(devices: Arc<Mutex<HashSet<SocketAddr>>>) {
+async fn state(devices: Arc<Mutex<HashMap<SocketAddr, Device>>>) {
     println!(
         "🚀 Synche running on port {}. Press Ctrl+C to stop.",
         BROADCAST_PORT
     );
     loop {
-        tokio::time::sleep(Duration::from_secs(60)).await;
-        let devices = devices.lock().unwrap();
-        println!("Connected Synche devices: {:?}", devices);
+        tokio::time::sleep(Duration::from_secs(10)).await;
+
+        let mut devices = devices.lock().unwrap();
+        devices.retain(|_, device| !matches!(device.last_seen.elapsed(), Ok(elapsed) if elapsed.as_secs() > DEVICE_TIMEOUT_SECS));
+
+        if !devices.is_empty() {
+            println!(
+                "Connected Synche devices: {:?}",
+                devices.keys().collect::<Vec<_>>()
+            );
+        } else {
+            println!("No Synche devices connected.");
+        }
     }
 }
 
