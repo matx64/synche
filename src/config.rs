96f186893e607143edb03c101415dc17e9ecbfc9
Matheus Alves <mathalves@proton.me>
2025-07-03 19:36:16 -0300

    refactor config, add startup sha256 hash

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,9 @@
 use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
 use std::{
     collections::HashMap,
     fs::{self},
+    io::Read,
     path::Path,
     sync::{Arc, RwLock},
     time::SystemTime,
@@ -15,6 +17,7 @@ pub struct Config {
 pub struct SynchedFile {
     pub name: String,
     pub last_modified_at: SystemTime,
+    pub hash: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -23,35 +26,47 @@ pub struct ConfigSynchedFile {
 }
 
 pub fn init() -> Config {
-    let contents = fs::read_to_string(".cfg.json").expect("Failed to read .cfg.json");
+    let cfg_path = ".cfg.json";
+    let files_dir = "synche-files";
 
-    let files: Vec<ConfigSynchedFile> =
-        serde_json::from_str(&contents).expect("Failed to parse JSON");
+    let files = load_config_file(cfg_path);
+    fs::create_dir_all("synche-files").unwrap();
 
-    let _ = fs::create_dir_all("synche-files");
+    Config {
+        synched_files: Arc::new(RwLock::new(build_synched_files(files, files_dir))),
+    }
+}
 
-    let mut existing_files = Vec::new();
+fn load_config_file(path: &str) -> Vec<ConfigSynchedFile> {
+    let contents = fs::read_to_string(path).expect("Failed to read config file");
+    serde_json::from_str(&contents).expect("Failed to parse config file")
+}
+
+fn build_synched_files(files: Vec<ConfigSynchedFile>, dir: &str) -> HashMap<String, SynchedFile> {
+    let mut result = HashMap::new();
+
     for file in files {
-        let path = Path::new("synche-files").join(&file.name);
+        let path = Path::new(dir).join(&file.name);
 
         if let Ok(metadata) = fs::metadata(&path) {
             if metadata.is_file() {
                 if let Ok(last_modified_at) = metadata.modified() {
-                    existing_files.push(SynchedFile {
-                        name: file.name.clone(),
-                        last_modified_at,
-                    });
+                    let mut f = fs::File::open(&path).unwrap();
+                    let mut content = Vec::new();
+                    f.read_to_end(&mut content).unwrap();
+
+                    result.insert(
+                        file.name.clone(),
+                        SynchedFile {
+                            name: file.name,
+                            hash: format!("{:x}", Sha256::digest(content)),
+                            last_modified_at,
+                        },
+                    );
                 }
             }
         }
     }
 
-    let synched_files = existing_files
-        .into_iter()
-        .map(|f| (f.name.clone(), f))
-        .collect::<HashMap<_, _>>();
-
-    Config {
-        synched_files: Arc::new(RwLock::new(synched_files)),
-    }
+    result
 }
