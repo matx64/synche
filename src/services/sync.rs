0887a4afc3823fc4fea8f1d2ec5d96cf43c45e0c
Matheus Alves <mathalves@proton.me>
2025-07-17 08:53:46 -0300

    more sync events refactors

--- src/services/sync.rs
+++ src/services/sync.rs
@@ -1,6 +1,9 @@
 use crate::{
     config::AppState,
-    models::{entry::File, sync::SyncDataKind},
+    models::{
+        entry::File,
+        sync::{SyncFileKind, SyncKind},
+    },
     services::{file::FileService, handshake::HandshakeService},
 };
 use std::{collections::HashMap, sync::Arc, time::Duration};
@@ -41,67 +44,111 @@ impl SyncService {
             let mut kind_buf = [0u8; 1];
             stream.read_exact(&mut kind_buf).await?;
 
-            let kind = SyncDataKind::try_from(kind_buf[0])?;
+            let kind = SyncKind::try_from(kind_buf[0])?;
             info!("Received {} from {}", kind, addr.ip());
 
             match kind {
-                SyncDataKind::HandshakeRequest => {
+                SyncKind::Handshake(kind) => {
                     self.handshake_service
-                        .read_handshake(&mut stream, true)
+                        .read_handshake(&mut stream, kind)
                         .await?;
                 }
-                SyncDataKind::HandshakeResponse => {
-                    self.handshake_service
-                        .read_handshake(&mut stream, false)
-                        .await?;
+                SyncKind::File(SyncFileKind::Metadata) => {
+                    self.handle_file_metadata(&mut stream).await?;
                 }
-                SyncDataKind::FileTransfer => {
+                SyncKind::File(SyncFileKind::Request) => {
+                    self.handle_file_request(&mut stream).await?
+                }
+                SyncKind::File(SyncFileKind::Transfer) => {
                     self.handle_file_transfer(&mut stream).await?;
-                }
-                SyncDataKind::FileRemoved => {
-                    self.handle_file_removed(&mut stream).await?;
                 }
             };
         }
     }
 
+    async fn handle_file_metadata(&self, stream: &mut TcpStream) -> std::io::Result<()> {
+        let src_addr = stream.peer_addr()?;
+        let src_ip = src_addr.ip();
+
+        let peer_file = self.file_service.read_metadata(stream).await?;
+
+        let is_deleted = peer_file.is_deleted();
+        if is_deleted {
+            self.state
+                .peer_manager
+                .remove_file(&src_ip, &peer_file.name);
+        } else {
+            self.state
+                .peer_manager
+                .insert_file(&src_ip, peer_file.clone());
+        }
+
+        match self.state.entry_manager.get_file(&peer_file.name) {
+            Some(local_file) => {
+                if local_file.last_modified_at < peer_file.last_modified_at {
+                    if is_deleted {
+                        self.file_service.remove_file(&peer_file.name).await;
+                        self.file_service
+                            .send_metadata(&File::absent(peer_file.name), src_addr)
+                            .await?;
+                    } else if local_file.hash != peer_file.hash {
+                        self.file_service
+                            .send_request(&peer_file.name, src_addr)
+                            .await?;
+                    }
+                }
+            }
+
+            None => {
+                if !is_deleted {
+                    self.state.entry_manager.insert_file(peer_file.clone());
+                    self.file_service
+                        .send_metadata(&peer_file, src_addr)
+                        .await?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn handle_file_request(&self, stream: &mut TcpStream) -> std::io::Result<()> {
+        let src_addr = stream.peer_addr()?;
+
+        let file_name = self.file_service.read_request(stream).await?;
+
+        if let Some(file) = self.state.entry_manager.get_file(&file_name) {
+            self.file_service.send_file(&file, src_addr).await?;
+        }
+        Ok(())
+    }
+
     async fn handle_file_transfer(&self, stream: &mut TcpStream) -> std::io::Result<()> {
-        let src_ip = stream.peer_addr()?.ip();
+        let src_addr = stream.peer_addr()?;
 
         let recv_file = self.file_service.read_file(stream).await?;
+        let file = self.file_service.save_file(&recv_file).await?;
 
-        self.file_service.save_file(&src_ip, &recv_file).await?;
+        self.file_service.send_metadata(&file, src_addr).await?;
 
         info!(
             "Successfully handled FileTransfer: {} ({} bytes) from {}",
-            recv_file.name, recv_file.size, src_ip
+            recv_file.name,
+            recv_file.size,
+            src_addr.ip()
         );
         Ok(())
     }
 
-    async fn handle_file_removed(&self, stream: &mut TcpStream) -> std::io::Result<()> {
-        let src_ip = stream.peer_addr()?.ip();
-
-        let filename = self.file_service.read_file_removed(stream).await?;
-
-        self.file_service.remove_file(&src_ip, &filename).await;
-
-        info!(
-            "Successfully removed file: {} | Origin: {}",
-            filename, src_ip
-        );
-        Ok(())
-    }
-
-    pub async fn sync_files(&self, mut sync_rx: Receiver<File>) -> io::Result<()> {
-        let mut buffer = HashMap::<String, File>::new();
+    pub async fn sync_files(&self, mut sync_rx: Receiver<(SyncFileKind, File)>) -> io::Result<()> {
+        let mut buffer = HashMap::<String, (SyncFileKind, File)>::new();
         let mut interval = time::interval(Duration::from_secs(5));
 
         loop {
             tokio::select! {
-                Some(file) = sync_rx.recv() => {
+                Some((kind, file)) = sync_rx.recv() => {
                     info!("File added to buffer: {}", file.name);
-                    buffer.insert(file.name.clone(), file);
+                    buffer.insert(file.name.clone(), (kind, file));
                 }
 
                 _ = interval.tick() => {
