7082e26ed19e86d418502ed2c0ff972fc514a5b2
Matheus Alve <mathalves@proton.me>
2025-10-28 10:32:50 -0300

    update peer status on disconnect

--- gui/static/sse.js
+++ gui/static/sse.js
@@ -13,16 +13,30 @@ es.onmessage = (event) => {
     case "PeerConnected":
       render_peer_list_item_component(payload);
       break;
+
+    case "PeerDisconnected":
+      disconnect_peer(payload);
+      break;
   }
 };
 
 function render_peer_list_item_component({ id, addr, hostname }) {
+  document.getElementById(`peer-${id}`)?.remove();
+
   const component = `<details class="list-item" id="peer-${id}">
             <summary><strong>🖥️ ${hostname}</strong></summary>
-            <p><strong>Status: </strong> 🟢 Connected</p>
+            <p><strong>Status: </strong> <span class="peer-status">🟢 Connected</span></p>
             <p><strong>IP:</strong> ${addr}</p>
             <p><strong>ID:</strong> ${id}</p>
           </details>`;
 
   el_peer_list.insertAdjacentHTML("beforeend", component);
+}
+
+function disconnect_peer(id) {
+  const el = document.getElementById(`peer-${id}`);
+  if (!el) return;
+
+  const status = el.querySelector(".peer-status");
+  if (status) status.innerHTML = "🔴 Disconnected";
 }
