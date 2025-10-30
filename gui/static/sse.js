333c498b7a913f2aa6272bd11ba5111216946010
Matheus Alves <mathalves@proton.me>
2025-10-30 14:45:34 -0300

    peer status style changes

--- gui/static/sse.js
+++ gui/static/sse.js
@@ -30,7 +30,16 @@ function render_peer_list_item_component({id, addr, hostname}) {
                         <rect height="12" rx="2" ry="2" width="18" x="3" y="4"/>
                         <line x1="2" x2="22" y1="20" y2="20"/>
                     </svg><span>${hostname}</span></strong
-                    ><small class="peer-status">🟢 Connected</small></summary>
+                    ><small class="peer-status">
+                    <span>Connected</span>
+                    <svg class="lucide lucide-cloud-check-icon lucide-cloud-check connected" fill="none" height="17"
+                         stroke="currentColor" stroke-linecap="round"
+                         stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="17"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path d="m17 15-5.5 5.5L9 18"/>
+                        <path d="M5 17.743A7 7 0 1 1 15.71 10h1.79a4.5 4.5 0 0 1 1.5 8.742"/>
+                    </svg>
+                </small></summary>
             <p><strong>IP:</strong> ${addr}</p>
             <p><strong>ID:</strong> ${id}</p>
           </details>`;
@@ -43,5 +52,8 @@ function disconnect_peer(id) {
     if (!el) return;
 
     const status = el.querySelector(".peer-status");
-    if (status) status.innerHTML = "🔴 Disconnected";
+    if (status) {
+        status.innerHTML = `<span>Disconnected</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cloud-alert-icon lucide-cloud-alert disconnected"><path d="M12 12v4"/><path d="M12 20h.01"/><path d="M17 18h.5a1 1 0 0 0 0-9h-1.79A7 7 0 1 0 7 17.708"/></svg>`;
+    }
 }
